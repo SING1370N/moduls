90b5246e952c0586c3d2740ecc1ce86882ccaf9c
sky.has.no.limits <69756932+SING1370N@users.noreply.github.com>
2023-05-22 12:01:06 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,12 +1,85 @@
 from .. import loader
-import random
-import math
-from asyncio import sleep
+import operator, math
+
+
 @loader.tds
 class HeartsMod(loader.Module):
-	strings = {"name": "Cacl"}
-	@loader.owner
-	async def calccmd(self, message):
-				await message.edit("В процессі розробки.")
+    strings = {"name": "Cacl"}
+    
+    @loader.owner
+    async def calccmd(self, message):
+        OPS = {
+            '-': operator.sub,
+            '+': operator.add,
+            '*': operator.mul,
+            '/': operator.truediv,
+        }
+        FUNCS = {
+            'sin': math.sin,
+            'cos': math.cos,
+        }
 
-		
+        def calc(token):
+            try:
+                return float(token)
+
+            except ValueError:
+                begin = token.find('(')
+                if begin == 0 and token[-1] == ')':
+                    return parse(token[1:-1])
+
+                elif begin > 0 and token[-1] == ')':
+                    name = token[0:begin]
+                    if name not in FUNCS:
+                        raise ValueError('Unknown function %s' % name)
+                    result = FUNCS[name](parse(token[begin + 1:-1]))
+                    return result
+
+                else:
+                    raise ValueError('Unknown token %s' % repr(token))
+
+        def parse(txt):
+            token = ''
+            depth = 0
+            op = None
+            current_value = None
+
+            def close_token():
+                nonlocal token, op, current_value
+                if token != '':
+                    if current_value is None:
+                        current_value = calc(token)
+                    else:
+                        current_value = OPS[op](current_value, calc(token))
+                        op = None
+                    print(op, token, current_value)
+                    token = ''
+
+            for c in txt:
+                if depth > 0:
+                    if c == ' ':
+                        continue
+                    token += c
+                    if c == ')':
+                        depth -= 1
+                        close_token()
+
+                elif c == '(':
+                    if token == '':
+                        close_token()
+                    depth += 1
+                    token += c
+
+                else:
+                    if c == ' ':
+                        continue
+                    elif c in OPS:
+                        close_token()
+                        op = c
+                    else:
+                        token += c
+
+            close_token()
+            return current_value
+        
+        await message.edit("COMMAND: " + message)
