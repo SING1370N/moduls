b6693fce2cd94db7f9dd69da9b3796828f388a70
sky.has.no.limits <69756932+SING1370N@users.noreply.github.com>
2023-05-22 12:52:21 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,10 +4,17 @@ import operator, math
 
 @loader.tds
 class CalcMod(loader.Module):
-    strings = {"name": "Cacl"}
+    strings = {"name": "Cacl",
+              "no_term": "<b>Нічого не знайдено</b>"}
     
     @loader.owner
     async def calccmd(self, message):
+        text = utils.get_args_raw(message.message)
+        if not text:
+            text = (await message.get_reply_message()).message
+        if not text:
+            await utils.answer(message, self.strings("no_term", message))
+            return
         OPS = {
             '-': operator.sub,
             '+': operator.add,
@@ -82,4 +89,4 @@ class CalcMod(loader.Module):
             close_token()
             return current_value
         
-        await message.edit("COMMAND: " + message.message)
+        await message.edit("COMMAND: " + text)
