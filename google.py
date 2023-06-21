021dddaac44fc210891e15f14c0d88b36127b12c
sky.has.no.limits <69756932+SING1370N@users.noreply.github.com>
2023-06-21 09:38:18 +0300

    Update google.py

--- google.py
+++ google.py
@@ -20,7 +20,7 @@ class GoogleSearchMod(loader.Module):
                "no_term": "<b>Нічого не знайдено</b>",
                "error": "<b>Сталася дивна помилка, скоріш за все КАПЧА</b>",
                "no_results": "<b>Не вдалося знайти нічого про</b> <code>{}</code> <b>в Google</b>",
-               "results": "<b>Це отримано з пошуку Google</b> <code>{}</code>:\n\n",
+               "results": "<b>Це отримано з пошуку {}</b> <code>{}</code>:\n\n",
                "result": "<a href='{}'>{}</a>\n\n<code>{}</code>\n"}
 
     @loader.unrestricted
@@ -43,7 +43,7 @@ class GoogleSearchMod(loader.Module):
             for result in all_results:
                 msg += self.strings("result", message).format(utils.escape_html(result[0]), utils.escape_html(result[1]),
                                                               utils.escape_html(result[2]))
-            await utils.answer(message, self.strings("results", message).format(utils.escape_html(text)) + msg)
+            await utils.answer(message, self.strings("results", CoreSearch.__name__, message).format(utils.escape_html(text)) + msg)
         except Exception:
             await utils.answer(message, self.strings("error", message))
             return
